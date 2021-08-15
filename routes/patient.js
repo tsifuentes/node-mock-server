1887657200a04fef73db46f6a3ff3fb2f31cc9bb
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-14 22:41:27 -0500

    feat: add uuid for patient id an get mem data

--- routes/patient.js
+++ routes/patient.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const dbUtil = require('../mock/dbutil');
 var router = express.Router({ mergeParams: true });
 var patient = require('../mock/patient.json');
 var patients = require('../mock/patients.json');
 
 router.get('/', (req, res, next) => {
-    res.status(200).send(req.params && req.params.id ? patient : patients);
+    if(req.params && !req.query.mem) {
+        res.status(200).send(req.params && req.params.id ? patient : patients);
+    } else {
+        res.status(200).json(dbUtil);
+    }
 });
 module.exports = router;
--- routes/save-patient.js
+++ routes/save-patient.js
@@ -1,9 +1,11 @@
 var express = require('express');
 const dbUtil = require('../mock/dbutil');
 var router = express.Router();
+const { uuid } = require('uuidv4');
 
 router.post('/', (req, res, next) => {
     dbUtil.data.push({
+        id: uuid(),
         ...req.body
     });
     res.status(200).send();
