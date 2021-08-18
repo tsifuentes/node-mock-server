f4e66b620c7e340c8370ea3d02f13dd76fd617cc
Thomas Sifuentes <smthomas1203@gmail.com>
2021-08-17 21:46:07 -0500

    feat: restructuring patient for citame app

--- routes/patient.js
+++ routes/patient.js
@@ -9,7 +9,7 @@ router.get('/:id', (req, res, next) => {
     if(req.params && !req.query.mem) {
         res.status(200).send(patient);
     } else {
-        res.status(200).json(dbUtil.data.find((patient) => patient.id = req.params.id));
+        res.status(200).json(dbUtil.citame.patient.data.find((patient) => patient.id = req.params.id));
     }
 });
 
@@ -17,13 +17,13 @@ router.get('/', (req, res, next) => {
     if(!req.query.mem) {
         res.status(200).send(patients);
     } else {
-        res.status(200).json(dbUtil);
+        res.status(200).json(dbUtil.citame.patient);
     }
 });
 
 router.post('/', (req, res, next) => {
     const id = uuid();
-    dbUtil.data.push({
+    dbUtil.citame.patient.data.push({
         id,
         ...req.body
     });
@@ -31,13 +31,13 @@ router.post('/', (req, res, next) => {
 });
 
 router.put('/:id', (req, res, next) => {
-    const patient = dbUtil.data.find((patient) => patient.id = req.params.id);
+    const patient = dbUtil.citame.patient.data.find((patient) => patient.id = req.params.id);
     Object.assign(patient, req.body);
     res.status(200).send();
 });
 
 router.delete('/:id', (req, res, next) => {
-    dbUtil.data = dbUtil.data.filter(patient => patient.id !== req.params.id);
+    dbUtil.citame.patient.data = dbUtil.citame.patient.data.filter(patient => patient.id !== req.params.id);
     res.status(200).send();
 });
 
